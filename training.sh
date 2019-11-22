0d2d19fd1b27139a2c591597dbf2e359a1adacd9
Esteban AP <estape11@gmail.com>
2019-11-22 13:03:07 -0600

    Script de test para VM y training

--- training.sh
+++ training.sh
@@ -1,6 +1,17 @@
 # Execution
 # Example > python3 Training.py Model Epoch BatchSize BatchSizeTest Steps ValidationSteps
 
+DIR="patrones"
+if [ -d "$DIR" ]; then
+	cd patrones
+	git checkout EntrenamientoServidor
+else
+	# Clonacion
+	git clone https://estape11:thekillers97@gitlab.com/debruynmonge/patrones.git
+	cd patrones
+	git checkout EntrenamientoServidor
+fi
+
 python3 Training.py 3 4 100 30 1000 200
 python3 Training.py 3 4 100 30 1000 200
 python3 Training.py 3 4 100 30 1000 200
