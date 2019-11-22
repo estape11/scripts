ba37a1ec18e5b2badd64c87f87ce6fc39e0dabd1
Esteban AP <estape11@gmail.com>
2019-11-22 13:14:12 -0600

    Script de test para VM y training

--- training.sh
+++ training.sh
@@ -1,6 +1,7 @@
 # Execution
 # Example > python3 Training.py Model Epoch BatchSize BatchSizeTest Steps ValidationSteps
 
+echo ">> Starting..."
 DIR="patrones"
 if [ -d "$DIR" ]; then
 	cd patrones
@@ -13,9 +14,17 @@ else
 fi
 
 python3 Training.py 3 4 100 30 1000 200
+git add * && git commit -m "Prueba 1.1" && git push origin EntrenamientoServidor
 python3 Training.py 3 4 100 30 1000 200
+git add * && git commit -m "Prueba 1.2" && git push origin EntrenamientoServidor
 python3 Training.py 3 4 100 30 1000 200
+git add * && git commit -m "Prueba 1.3" && git push origin EntrenamientoServidor
 python3 Training.py 3 4 100 30 1000 200
+git add * && git commit -m "Prueba 1.4" && git push origin EntrenamientoServidor
 
 python3 Training.py 4 4 200 30 700 200
+git add * && git commit -m "Prueba 2.1" && git push origin EntrenamientoServidor
 python3 Training.py 4 4 200 30 700 200
+git add * && git commit -m "Prueba 2.2" && git push origin EntrenamientoServidor
+
+echo ">> Finished"
