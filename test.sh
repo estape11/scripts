2f28437185c2f3189d426392a2b51b04c8721f4f
Esteban AP <estape11@gmail.com>
2019-11-22 09:56:19 -0600

    Script de test para VM y training

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Stop on the first sign of trouble
+set -e
+
+SCRIPT_DIR=$(pwd)
+
+if [ $UID != 0 ]; then
+    echo "Error: Ejecutar como root"
+    exit 1
+fi
+
+git clone https://estape11:thekillers97@gitlab.com/debruynmonge/patrones.git
+cd patrones
+git checkout ProyectoFinalJacoboVM
+
+apt-get update
+apt-get install python-pip -y
+apt-get install python-pip3 -y
+apt-get install libopencv-dev python-opencv
+
+pip3 install opencv-python --user
+pip install opencv-python --user
+
+pip3 install scikit-image --user
+pip install scikit-image --user
+
+pip3 install sklearn --user
+pip install sklearn --user
+
+pip3 install tensorflow --user
+pip install tensorflow --user
+
+pip3 install Keras --user
+pip install Keras --user
+
+python3 Training.py
