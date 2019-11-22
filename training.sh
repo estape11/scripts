26373a7d5c2de43b4f864a04ddf913e43b807f74
Esteban AP <estape11@gmail.com>
2019-11-22 12:57:34 -0600

    Script de test para VM y training

---
+++ training.sh
@@ -0,0 +1,10 @@
+# Execution
+# Example > python3 Training.py Model Epoch BatchSize BatchSizeTest Steps ValidationSteps
+
+python3 Training.py 3 4 100 30 1000 200
+python3 Training.py 3 4 100 30 1000 200
+python3 Training.py 3 4 100 30 1000 200
+python3 Training.py 3 4 100 30 1000 200
+
+python3 Training.py 4 4 200 30 700 200
+python3 Training.py 4 4 200 30 700 200
